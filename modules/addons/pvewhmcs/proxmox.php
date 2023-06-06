ea917b4c4683cf8d55899d6bf0bdda32a2e3eda4
Luke S Thompson <lst@luke.st>
2023-06-06 19:50:38 +1000

    Update PVE2 PHP Class from @cpuid

--- modules/addons/pvewhmcs/proxmox.php
+++ modules/addons/pvewhmcs/proxmox.php
@@ -1,6 +1,8 @@
 <?php
+
+class PVE2_Exception extends RuntimeException {}
+
 class PVE2_API {
-	protected $constructor_success = null;
 	protected $hostname;
 	protected $username;
 	protected $realm;
@@ -14,23 +16,19 @@ class PVE2_API {
 
 	public function __construct ($hostname, $username, $realm, $password, $port = 8006, $verify_ssl = false) {
 		if (empty($hostname) || empty($username) || empty($realm) || empty($password) || empty($port)) {
-			$this->constructor_success = false;
-			return false ;
+			throw new PVE2_Exception("Hostname/Username/Realm/Password/Port required for PVE2_API object constructor.", 1);
 		}
 		// Check hostname resolves.
 		if (gethostbyname($hostname) == $hostname && !filter_var($hostname, FILTER_VALIDATE_IP)) {
-			$this->constructor_success = false;
-			return false ;
+			throw new PVE2_Exception("Cannot resolve {$hostname}.", 2);
 		}
 		// Check port is between 1 and 65535.
-		if (!is_int($port) || $port < 1 || $port > 65535) {
-			$this->constructor_success = false;
-			return false ;
+		if (!filter_var($port, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1, 'max_range' => 65535]])) {
+			throw new PVE2_Exception("Port must be an integer between 1 and 65535.", 6);
 		}
 		// Check that verify_ssl is boolean.
 		if (!is_bool($verify_ssl)) {
-			$this->constructor_success = false;
-			return false ;
+			throw new PVE2_Exception("verify_ssl must be boolean.", 7);
 		}
 
 		$this->hostname   = $hostname;
@@ -62,6 +60,7 @@ class PVE2_API {
 		curl_setopt($prox_ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($prox_ch, CURLOPT_POSTFIELDS, $login_postfields_string);
 		curl_setopt($prox_ch, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
+		curl_setopt($prox_ch, CURLOPT_SSL_VERIFYHOST, $this->verify_ssl);
 
 		$login_ticket = curl_exec($prox_ch);
 		$login_request_info = curl_getinfo($prox_ch);
@@ -82,8 +81,7 @@ class PVE2_API {
 			// Just to be safe, set this to null again.
 			$this->login_ticket_timestamp = null;
 			if ($login_request_info['ssl_verify_result'] == 1) {
-				$this->constructor_success = false;
-				return false ;				
+				throw new PVE2_Exception("Invalid SSL cert on {$this->hostname} - check that the hostname is correct, and that it appears in the server certificate's SAN list. Alternatively set the verify_ssl flag to false if you are using internal self-signed certs (ensure you are aware of the security risks before doing so).", 4);
 			}
 			return false;
 		} else {
@@ -103,8 +101,7 @@ class PVE2_API {
 	# Use with care, and DO NOT use with root, it may harm your system
 	public function setCookie() {
 		if (!$this->check_login_ticket()) {
-			$this->constructor_success = false;
-			return false ;
+			throw new PVE2_Exception("Not logged into Proxmox host. No Login access ticket found or ticket expired.", 3);
 		}
 
 		setrawcookie("PVEAuthCookie", $this->login_ticket['ticket'], 0, "/");
@@ -143,8 +140,7 @@ class PVE2_API {
 		}
 
 		if (!$this->check_login_ticket()) {
-			$this->constructor_success = false;
-			return false ;
+			throw new PVE2_Exception("Not logged into Proxmox host. No Login access ticket found or ticket expired.", 3);
 		}
 
 		// Prepare cURL resource.
@@ -157,7 +153,6 @@ class PVE2_API {
 		switch ($http_method) {
 			case "GET":
 				// Nothing extra to do.
-				curl_setopt($prox_ch, CURLOPT_URL, "https://{$this->hostname}:{$this->port}/api2/json{$action_path}?".http_build_query($put_post_parameters));
 				break;
 			case "PUT":
 				curl_setopt($prox_ch, CURLOPT_CUSTOMREQUEST, "PUT");
@@ -189,7 +184,7 @@ class PVE2_API {
 				curl_setopt($prox_ch, CURLOPT_HTTPHEADER, $put_post_http_headers);
 				break;
 			default:
-				//throw new PVE2_Exception("Error - Invalid HTTP Method specified.", 5);	
+				throw new PVE2_Exception("Error - Invalid HTTP Method specified.", 5);
 				return false;
 		}
 
@@ -197,6 +192,7 @@ class PVE2_API {
 		curl_setopt($prox_ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($prox_ch, CURLOPT_COOKIE, "PVEAuthCookie=".$this->login_ticket['ticket']);
 		curl_setopt($prox_ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($prox_ch, CURLOPT_SSL_VERIFYHOST, false);
 
 		$action_response = curl_exec($prox_ch);
 
@@ -230,8 +226,8 @@ class PVE2_API {
 					return $action_response_array['data'];
 				}
 			} else {
-				error_log("This API Request Failed.\n" . 
-					"HTTP Response - {$split_http_response_line[1]}\n" . 
+				error_log("This API Request Failed.\n" .
+					"HTTP Response - {$split_http_response_line[1]}\n" .
 					"HTTP Error - {$split_headers[0]}");
 				return false;
 			}
@@ -243,7 +239,7 @@ class PVE2_API {
 		if (!empty($action_response_array['data'])) {
 			return $action_response_array['data'];
 		} else {
-			error_log("\$action_response_array['data'] is empty. Returning false.\n" . 
+			error_log("\$action_response_array['data'] is empty. Returning false.\n" .
 				var_export($action_response_array['data'], true));
 			return false;
 		}
@@ -284,7 +280,7 @@ class PVE2_API {
 
 		return $this->cluster_node_list;
 	}
-	
+
 	/*
 	 * bool|int get_next_vmid ()
 	 * Get Last VMID from a Cluster or a Node
@@ -299,6 +295,229 @@ class PVE2_API {
 		}
 	}
 
+	/*
+	 * array get_vms ()
+	 * Get List of all vms
+	 */
+	public function get_vms () {
+		$node_list = $this->get_node_list();
+		$result=[];
+		if (count($node_list) > 0) {
+			foreach ($node_list as $node_name) {
+				$vms_list = $this->get("nodes/" . $node_name . "/qemu/");
+				if (count($vms_list) > 0) {
+					$key_values = array_column($vms_list, 'vmid'); 
+					array_multisort($key_values, SORT_ASC, $vms_list);
+					foreach($vms_list as &$row) {
+						$row[node] = $node_name;
+					}
+					$result = array_merge($result, $vms_list);
+				}
+				if (count($result) > 0) {
+					$this->$cluster_vms_list = $result;
+					return $this->$cluster_vms_list;
+				} else {
+					error_log(" Empty list of vms returned in this cluster.");
+					return false;
+				}
+			}
+		} else {
+			error_log(" Empty list of nodes returned in this cluster.");
+			return false;
+		}
+	}
+	
+	/*
+	 * bool|int start_vm ($node,$vmid)
+	 * Start specific vm
+	 */
+	public function start_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$parameters = array(
+				"vmid" => $vmid,
+				"node" => $node,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/status/start";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Started vm " . $vmid . "");
+				return true;
+			} else {
+				error_log("Error starting vm " . $vmid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+	
+	/*
+	 * bool|int shutdown_vm ($node,$vmid)
+	 * Gracefully shutdown specific vm
+	 */
+	public function shutdown_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$parameters = array(
+				"vmid" => $vmid,
+				"node" => $node,
+				"timeout" => 60,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/status/shutdown";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Shutdown vm " . $vmid . "");
+				return true;
+			} else {
+				error_log("Error shutting down vm " . $vmid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+
+	/*
+	 * bool|int stop_vm ($node,$vmid)
+	 * Force stop specific vm
+	 */
+	public function stop_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$parameters = array(
+				"vmid" => $vmid,
+				"node" => $node,
+				"timeout" => 60,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/status/stop";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Stopped vm " . $vmid . "");
+				return true;
+			} else {
+				error_log("Error stopping vm " . $vmid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+	
+	/*
+	 * bool|int resume_vm ($node,$vmid)
+	 * Resume from suspend specific vm
+	 */
+	public function resume_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$parameters = array(
+				"vmid" => $vmid,
+				"node" => $node,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/status/resume";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Resumed vm " . $vmid . "");
+				return true;
+			} else {
+				error_log("Error resuming vm " . $vmid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+	
+	/*
+	 * bool|int suspend_vm ($node,$vmid)
+	 * Suspend specific vm
+	 */
+	public function suspend_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$parameters = array(
+				"vmid" => $vmid,
+                		"node" => $node,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/status/suspend";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Suspended vm " . $vmid . "");
+				return true;
+			} else {
+				error_log("Error suspending vm " . $vmid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+	
+	/*
+	 * bool|int clone_vm ($node,$vmid)
+	 * Create fullclone of vm
+	 */
+	public function clone_vm ($node,$vmid) {
+		if(isset($vmid) && isset($node)){
+			$lastid = $this->get_next_vmid();
+			$parameters = array(
+				"vmid" => $vmid,
+				"node" => $node,
+				"newid" => $lastid,
+				"full" => true,
+			);
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/clone";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Cloned vm " . $vmid . " to " . $lastid . "");
+				return true;
+			} else {
+				error_log("Error cloning vm " . $vmid . " to " . $lastid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+
+	/*
+	 * bool|int snapshot_vm ($node,$vmid,$snapname = NULL)
+	 * Create snapshot of vm
+	 */	
+	public function snapshot_vm ($node,$vmid,$snapname = NULL) {
+		if(isset($vmid) && isset($node)){
+			$lastid = $this->get_next_vmid();
+			if (is_null($snapname)){
+				$parameters = array(
+					"vmid" => $vmid,
+					"node" => $node,
+					"vmstate" => true,
+				);
+			} else {
+				$parameters = array(
+					"vmid" => $vmid,
+					"node" => $node,
+					"vmstate" => true,
+					"snapname" => $snapname,
+				);
+			}
+			$url = "/nodes/" . $node . "/qemu/" . $vmid . "/snapshot";
+			$post = $this->post($url,$parameters);
+			if ($post) {
+				error_log("Cloned vm " . $vmid . " to " . $lastid . "");
+				return true;
+			} else {
+				error_log("Error cloning vm " . $vmid . " to " . $lastid . "");
+				return false;
+			}
+		} else {
+			error_log("no vm or node valid");
+			return false;
+		}
+	}
+	
 	/*
 	 * bool|string get_version ()
 	 * Return the version and minor revision of Proxmox Server
@@ -315,8 +534,8 @@ class PVE2_API {
 	/*
 	 * object/array? get (string action_path)
 	 */
-	public function get ($action_path,$parameters='null') {
-		return $this->action($action_path, "GET",$parameters);
+	public function get ($action_path) {
+		return $this->action($action_path, "GET");
 	}
 
 	/*
@@ -342,6 +561,5 @@ class PVE2_API {
 
 	// Logout not required, PVEAuthCookie tokens have a 2 hour lifetime.
 }
-
 
 ?>
